4bc761b552913185ef725dfed44bc5a3607be9cf
Lorsuz <ariel2005souza@gmail.com>
2023-11-13 19:43:38 -0300

    FINAL

---
+++ components/js/classes/SwiperConfigSponsors.js
@@ -0,0 +1,34 @@
+const SwiperConfigSponsors = {
+	direction: 'horizontal',
+	loop: true,
+	slidesPerView: 1,
+	grabCursor: true,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+	mousewheel: {
+		forceToAxis: true,
+	},
+	breakpoints: {
+		800: {
+			slidesPerView: 2,
+		},
+		1200: {
+			slidesPerView: 3,
+		}
+	},
+};
+
+export default SwiperConfigSponsors;
---
+++ components/js/imports/home.js
@@ -0,0 +1,2 @@
+import "../scripts/SongPlayer.js";
+import "../APIs/Slides.js";
